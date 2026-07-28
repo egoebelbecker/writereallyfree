43fe3d1d8dda2dd3503c35c36a009bb60c73516e
Eric Goebelbecker <eric@ericgoebelbecker.com>
2026-07-28 16:03:35 -0400

    MMmph.

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -1,6 +1,17 @@
+import sys
 from windows import get_windows_volume_label
 
 
-def test_windows_volume_label_none():
-    # On non-windows systems this should fail gracefully and return None
-    assert get_windows_volume_label('C:\\') is None
+def test_windows_volume_label():
+    if sys.platform != 'win32':
+        # On non-Windows systems, this should fail gracefully and return None
+        assert get_windows_volume_label('C:\\') is None
+    else:
+        # On Windows, C:\ returns a string volume label or None
+        label = get_windows_volume_label('C:\\')
+        assert label is None or isinstance(label, str)
+
+
+def test_windows_volume_label_invalid_drive():
+    # Non-existent drive path returns None on all platforms
+    assert get_windows_volume_label('Z:\\NonExistentPath') is None
